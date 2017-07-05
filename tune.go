c878abfbb7557aca48a51493bf598764370cea17
AbhishekKr <abhikumar163@gmail.com>
2017-07-06 02:44:11 +0530

    [tunefind] search for movie-artist songs, tv seasons
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- tune.go
+++ tune.go
@@ -21,6 +21,10 @@ type TunefindSearcResult struct {
 	RelUrl string
 }
 
+type TunefindSearcResults struct {
+	Results []TunefindSearcResult
+}
+
 func (searchResult TunefindSearcResult) LinkType() string {
 	urlTypeRegex, _ := regexp.Compile("^/([A-Za-z]*)/")
 	urlType := urlTypeRegex.FindStringSubmatch(searchResult.RelUrl)[1]
@@ -72,22 +76,90 @@ func IsArtistOnTunefind(artist string) bool {
 	return LinkExists(artistUrl)
 }
 
-func TunefindSearch(searchQuery string, searchType string) {
-	doc, err := goquery.NewDocument(TunefindUrlFor("search", searchQuery))
+func GoqueryDocument(url string) *goquery.Document {
+	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return doc
+}
 
-	doc.Find("div.row.tf-search-results a").Each(func(i int, s *goquery.Selection) {
-		href, hrefPresent := s.Attr("href")
-		searchResult := TunefindSearcResult{RelUrl: href}
-		if !hrefPresent {
-			return
+func GoqueryAttrsFrom(url string, goquerySelector string, attr string) (results TunefindSearcResults) {
+	doc := GoqueryDocument(url)
+
+	a_nodes := doc.Find(goquerySelector)
+	results.Results = make([]TunefindSearcResult, a_nodes.Size())
+	a_nodes.Each(func(i int, s *goquery.Selection) {
+		var attrValue string
+		var attrPresent bool
+		if attr == "text" {
+			attrValue = s.Text()
+			attrPresent = (attrValue != "")
+		} else {
+			attrValue, attrPresent = s.Attr(attr)
 		}
-		if searchType == searchResult.LinkType() || searchType == "all" {
-			fmt.Println(searchResult.RelUrl)
+		if !attrPresent {
+			s_html, _ := s.Html()
+			log.Printf("[warn] %s\n", s_html)
 		}
+
+		results.Results[i] = TunefindSearcResult{RelUrl: attrValue}
 	})
+	return
+}
+
+func GoqueryHrefsFrom(url string, goquerySelector string) (results TunefindSearcResults) {
+	return GoqueryAttrsFrom(url, goquerySelector, "href")
+}
+
+func GoqueryTextFrom(url string, goquerySelector string) (results TunefindSearcResults) {
+	return GoqueryAttrsFrom(url, goquerySelector, "text")
+}
+
+func TunefindSearch(searchQuery string, searchType string) {
+	fullUrl := TunefindUrlFor("search", searchQuery)
+	goquerySelector := "div.row.tf-search-results a"
+	for _, result := range GoqueryHrefsFrom(fullUrl, goquerySelector).Results {
+		if searchType != result.LinkType() && searchType != "all" {
+			continue
+		}
+
+		fmt.Println("[*]", result.RelUrl)
+		if result.LinkType() == "tv" {
+			TunefindTv(result.RelUrl)
+		} else if result.LinkType() == "movie" {
+			TunefindMovie(result.RelUrl)
+		} else if result.LinkType() == "artist" {
+			TunefindArtist(result.RelUrl)
+		}
+	}
+}
+
+func TunefindMovie(relUrl string) {
+	fullUrl := fmt.Sprintf("%s%s", tunefindBaseUrl, relUrl)
+
+	goquerySelector := "div.Tunefind__Content div.SongRow__center___1I0Cg h4.SongTitle__heading___3kxXK a"
+	for _, result := range GoqueryHrefsFrom(fullUrl, goquerySelector).Results {
+		fmt.Println(result.RelUrl)
+	}
+}
+
+func TunefindTv(relUrl string) {
+	fullUrl := fmt.Sprintf("%s%s", tunefindBaseUrl, relUrl)
+
+	goquerySelector := "div.Tunefind__Content ul[aria-labelledby='season-dropdown'] a[role='menuitem']"
+	for _, result := range GoqueryHrefsFrom(fullUrl, goquerySelector).Results {
+		fmt.Println(result.RelUrl)
+	}
+}
+
+func TunefindArtist(relUrl string) {
+	fullUrl := fmt.Sprintf("%s%s", tunefindBaseUrl, relUrl)
+
+	goquerySelector := "div.Tunefind__Content div.AppearanceRow__songInfoTitleBlock___3woDL div.AppearanceRow__songInfoTitle___38aKt"
+	for _, result := range GoqueryTextFrom(fullUrl, goquerySelector).Results {
+		fmt.Println(result.RelUrl)
+	}
 }
 
 func main() {
