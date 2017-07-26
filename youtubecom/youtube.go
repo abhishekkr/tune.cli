ae4ce8992bc25a722320917258b5b037b2e8e143
AbhishekKr <abhikumar163@gmail.com>
2017-07-27 04:08:29 +0530

    [youtube] FirstLink, Links updated to use GolGoquery with exception handled
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- youtubecom/youtube.go
+++ youtubecom/youtube.go
@@ -2,6 +2,7 @@ package tunecli_youtubecom
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/abhishekkr/gol/golgoquery"
 )
@@ -12,13 +13,21 @@ var (
 
 func Links(url string) []string {
 	goquerySelector := "div#page-container div.branded-page-v2-body.branded-page-v2-primary-column-content ol li h3.yt-lockup-title a"
-	results := golgoquery.GoqueryHrefsFrom(url, goquerySelector).Results
-	for idx, result := range results {
-		results[idx] = fmt.Sprintf("%s%s", youtubeBaseUrl, result)
+	results, err := golgoquery.GoqueryHrefsFrom(url, goquerySelector)
+	if err != nil {
+		log.Println("[warn] error go-querying", url)
 	}
-	return results
+	for idx, result := range results.Results {
+		results.Results[idx] = fmt.Sprintf("%s%s", youtubeBaseUrl, result)
+	}
+	return results.Results
 }
 
 func FirstLink(url string) string {
-	return Links(url)[0]
+	links := Links(url)
+	if len(links) == 0 {
+		log.Println("[error] no youtube url found for", url)
+		return ""
+	}
+	return links[0]
 }
